22f2d5732e64251257e867c4a126acb51b5ceeff
Aleksandr Fedotov <mail@orderbynull.me>
2021-05-21 21:34:40 +0300

    Обновил buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -2,8 +2,5 @@
 
 buildVersion=$1
 imageRepository=679602379035.dkr.ecr.us-east-2.amazonaws.com/enterprise-dev-course
-imageName=enterprise-dev-course
 
-./gradlew bootBuildImage --imageName=$imageName
-docker tag $imageName $imageRepository:${buildVersion:=latest}
-
+./gradlew bootBuildImage --imageName=$imageRepository:${buildVersion:=latest}
